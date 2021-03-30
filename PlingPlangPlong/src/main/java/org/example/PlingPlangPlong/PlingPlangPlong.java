b59e540a477c09361c6786ec6fcb1f31545b5b5e
Xavier2a2 <abanjoko@spartaglobal.com>
2021-03-30 15:59:59 +0100

    Set up for Pling Plang Plong.

--- PlingPlangPlong/src/main/java/org/example/PlingPlangPlong/PlingPlangPlong.java
+++ PlingPlangPlong/src/main/java/org/example/PlingPlangPlong/PlingPlangPlong.java
@@ -1,4 +1,20 @@
 package org.example.PlingPlangPlong;
 
 public class PlingPlangPlong {
+
+    static String plingPlangPlong(int number){
+        String output = "";
+
+        if(number%3==0){
+            output += "Pling";
+        } else if(number%5==0){
+            output += "Plang";
+        }else if(number%7==0){
+            output += "Plong";
+        }
+
+        if (output.equals("")) return number +"";
+
+        return output;
+    }
 }
