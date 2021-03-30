c2c6f0eea2d276a308c88124067ba6fdb8227a8d
Xavier2a2 <abanjoko@spartaglobal.com>
2021-03-30 16:06:35 +0100

    Correction to PlingPlangPlong class method. Initial tests in main. All passed.

--- PlingPlangPlong/src/main/java/org/example/App.java
+++ PlingPlangPlong/src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import org.example.PlingPlangPlong.PlingPlangPlong;
+
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(PlingPlangPlong.calculateResult(3));
+        System.out.println(PlingPlangPlong.calculateResult(5));
+        System.out.println(PlingPlangPlong.calculateResult(7));
+        System.out.println(PlingPlangPlong.calculateResult(15));
+        System.out.println(PlingPlangPlong.calculateResult(21));
+        System.out.println(PlingPlangPlong.calculateResult(35));
+        System.out.println(PlingPlangPlong.calculateResult(105));
+        System.out.println(PlingPlangPlong.calculateResult(8));
     }
 }
--- PlingPlangPlong/src/main/java/org/example/PlingPlangPlong/PlingPlangPlong.java
+++ PlingPlangPlong/src/main/java/org/example/PlingPlangPlong/PlingPlangPlong.java
@@ -2,17 +2,18 @@ package org.example.PlingPlangPlong;
 
 public class PlingPlangPlong {
 
-    static String plingPlangPlong(int number){
+    public static String calculateResult(int number){
         String output = "";
 
         if(number%3==0){
             output += "Pling";
-        } else if(number%5==0){
+        }
+        if(number%5==0){
             output += "Plang";
-        }else if(number%7==0){
+        }
+        if(number%7==0){
             output += "Plong";
         }
-
         if (output.equals("")) return number +"";
 
         return output;
