71c62fa9afecc7ed33c65e5511c952d1f53d2f46
Xavier2a2 <abanjoko@spartaglobal.com>
2021-04-06 11:18:26 +0100

    Simplified test assertions.

--- PlingPlangPlong/src/test/java/com/sparta/aaron/AppTest.java
+++ PlingPlangPlong/src/test/java/com/sparta/aaron/AppTest.java
@@ -4,6 +4,7 @@ import com.sparta.aaron.PlingPlangPlong.PlingPlangPlong;
 import org.junit.Assert;
 import org.junit.Test;
 
+
 /**
  * Unit test for simple App.
  */
@@ -12,15 +13,39 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+
     @Test
-    public void plingPlangPlongTrueTests()
-    {
+    public void plingTest(){
+        Assert.assertEquals("Pling", PlingPlangPlong.calculateResult(3));
         Assert.assertEquals("Pling", PlingPlangPlong.calculateResult(6));
+        Assert.assertEquals("Pling", PlingPlangPlong.calculateResult(9));
+    }
+
+    @Test
+    public void plangTest(){
+        Assert.assertEquals("Plang", PlingPlangPlong.calculateResult(5));
         Assert.assertEquals("Plang", PlingPlangPlong.calculateResult(10));
+        Assert.assertEquals("Plang", PlingPlangPlong.calculateResult(20));
+    }
+
+    @Test
+    public void plongTest(){
+        Assert.assertEquals("Plong", PlingPlangPlong.calculateResult(7));
         Assert.assertEquals("Plong", PlingPlangPlong.calculateResult(14));
+        Assert.assertEquals("Plong", PlingPlangPlong.calculateResult(28));
+    }
+
+    @Test
+    public void combinationsTest()
+    {
         Assert.assertEquals("PlingPlang", PlingPlangPlong.calculateResult(15));
         Assert.assertEquals("PlingPlong", PlingPlangPlong.calculateResult(21));
         Assert.assertEquals("PlangPlong", PlingPlangPlong.calculateResult(35));
+    }
+
+    @Test
+    public void plingPlangPlongTest()
+    {
         Assert.assertEquals("PlingPlangPlong", PlingPlangPlong.calculateResult(210));
     }
 
